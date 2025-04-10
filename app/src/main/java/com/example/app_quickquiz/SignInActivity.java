938bba672b2d790c54b5183fbe4be391c92ad494
HaVanDat <hadat1132003@gmail.com>
2025-04-10 11:55:30 +0700

    Thực hiện commit để merge nhánh NguyenBachDiep

--- app/src/main/java/com/example/app_quickquiz/SignInActivity.java
+++ app/src/main/java/com/example/app_quickquiz/SignInActivity.java
@@ -8,6 +8,7 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.text.InputType;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,12 +16,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class SignInActivity extends AppCompatActivity{
 
     private Button btnSignUp,btnSignInBottom;
     private EditText txtEmail,txtPassword;
     private ImageView btnHintPassword;
-    private TextView txtForgotPassword;
+    private TextView txtForgotPassword,passwordWarning;
+    private FirebaseAuth mAuth;
     private boolean isVisible = false;
 
     @Override
@@ -34,6 +38,11 @@ public class SignInActivity extends AppCompatActivity{
         txtPassword = findViewById(R.id.txtPassword);
         btnHintPassword = findViewById(R.id.btnHintPassword);
         txtForgotPassword = findViewById(R.id.txtForgotPassword);
+        btnSignInBottom = findViewById(R.id.btnSignInBottom);
+        passwordWarning = findViewById(R.id.passwordWarning);
+
+        // khởi tạo Firebase
+        mAuth = FirebaseAuth.getInstance();
 
         btnSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,18 +52,6 @@ public class SignInActivity extends AppCompatActivity{
             }
         });
 
-        btnHintPassword.setOnClickListener(v ->{
-            if (isVisible) {
-                txtPassword.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
-                btnHintPassword.setImageResource(R.drawable.ic_eye_close);
-            } else {
-                txtPassword.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
-                btnHintPassword.setImageResource(R.drawable.eyepassword);
-            }
-            isVisible = !isVisible;
-            txtPassword.setSelection(txtPassword.getText().length());
-        } );
-
         txtForgotPassword.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -63,7 +60,33 @@ public class SignInActivity extends AppCompatActivity{
             }
         });
 
+
+        btnHintPassword.setOnClickListener(v ->eyeHintPassword());
+        btnSignInBottom.setOnClickListener(v -> SignIn());
     }
+     boolean isPasswordVisible = false;
+     private void eyeHintPassword(){
+         if (isPasswordVisible) {
+             // Ẩn mật khẩu
+             txtPassword.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+             btnHintPassword.setImageResource(R.drawable.ic_eye_close); // Mắt đóng
+             isPasswordVisible = false;
+         } else {
+             // Hiện mật khẩu
+             txtPassword.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+             btnHintPassword.setImageResource(R.drawable.eyepassword); // Mắt mở
+             isPasswordVisible = true;
+         }
+
+         txtPassword.setSelection(txtPassword.length());
+     }
+
+     private void SignIn() {
+         String email = txtEmail.getText().toString().trim();
+         String password = txtPassword.getText().toString().trim();
+
+
+     }
 }
 
 
